ec2a4b4efe8c3842836abcc2eb954f237d4fe559
Samir Maciel <samirmaciel.dev@gmail.com>
2022-02-13 16:26:32 -0300

    Created item for recyclerView with users

---
+++ app/src/main/java/com/example/queridometroapp/Feature/Modules/HomeScreen/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.queridometroapp.Feature.Modules.HomeScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.queridometroapp.R
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homescreen)
+    }
+}
---
+++ app/src/main/java/com/example/queridometroapp/Feature/Modules/HomeScreen/HomeScreenViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.queridometroapp.Feature.Modules.HomeScreen
+
+import androidx.lifecycle.ViewModel
+
+class HomeScreenViewModel : ViewModel(){
+
+
+}
